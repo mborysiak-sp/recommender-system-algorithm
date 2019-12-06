a8617f048c54e99515487ea28a7b03e93fe4b2db
kordianceranowski <kordianceranowski@gmail.com>
2019-12-06 01:33:54 +0100

    :<

---
+++ src/zabijcie mnie/wyrzynarka/Item.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wyrzynarka
+{
+    class Item : IComparable<Item>
+    {
+        
+        public int ItemID { get; set; }
+        public ArrayList Rates { get; set; }
+
+        public Item(int itemID, ArrayList rates)
+        {
+            ItemID = itemID;
+            Rates = rates;
+        }
+
+        public int CompareTo(Item other)
+        {
+            if (other == null) return 1;
+
+            return Rates.Count.CompareTo(other.Rates.Count);
+        }
+
+
+
+    }
+}
---
+++ src/zabijcie mnie/wyrzynarka/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace wyrzynarka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AllData allData = new AllData(30000);
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
