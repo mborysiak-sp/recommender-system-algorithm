cd95985d8fbe4a87a7408dd7e88841be2e66b30b
Marcin Borysiak <marcinez24@gmail.com>
2019-12-08 16:48:00 +0100

    Edit RMatrix, ALS, ObjectiveFunction
    Improved overall code readibility
    Removed redundant code from Add method in RMatrix

--- src/ALS.cs
+++ src/ALS.cs
@@ -4,107 +4,112 @@ using System.Text;
 
 namespace RecommenderSystem
 {
-    class ALS
-    {
-        private RMatrix R;
-        private Matrix U, P;
+	class ALS
+	{
+		private RMatrix R;
+		private Matrix U, P;
 
-        private int countOfFactors;
+		private int countOfFactors;
 
-        public ALS(int iloscFaktorow, int dawkaPliku, int iloscProduktowMacierzyR)
-        {
-            R = Extractor.createR(dawkaPliku, iloscProduktowMacierzyR);
-            U = new Matrix(iloscFaktorow, R.u);
-            U.FillRandom();
-            P = new Matrix(iloscFaktorow, R.p);
-            P.FillRandom();
-            countOfFactors = iloscFaktorow;
-        }
+		public ALS(int iloscFaktorow, int dawkaPliku, int iloscProduktowMacierzyR)
+		{
+			R = Extractor.createR(dawkaPliku, iloscProduktowMacierzyR);
 
-        public void Execute(double lambda, int numOfAlsIterations)
-        {
-            for (int k = 0; k < numOfAlsIterations; k++) 
-            {
-                for (int u = 0; u < R.u; u++) 
-                {
-                    List<int> productsRatedByU = R.FindAllProductsRatedByUser(u);
-                    
-                    Matrix Piu = new Matrix(countOfFactors, productsRatedByU.Count);
+			U = new Matrix(iloscFaktorow, R.u);
+			U.FillRandom();
 
-                    for (int i = 0; i < productsRatedByU.Count; i++) {
-                        for (int row = 0; row < countOfFactors; row++) {
-                            Piu.Data[row, i] = P.Data[row, productsRatedByU[i]];
-                        }
-                    }
-                    // W tym miejscu Piu jest gotowe do użytku
+			P = new Matrix(iloscFaktorow, R.p);
+			P.FillRandom();
 
-                    Matrix Au = Piu * Piu.GetTransposed();
-                    Au.AddLambdaMatrix(lambda);
-                    
-                    Matrix Vu = new Matrix(countOfFactors, 1);
+			countOfFactors = iloscFaktorow;
+		}
 
-                    for (int col = 0; col < Piu.ColumnCount; col++) 
-                    {
-                        int rating = R[u, productsRatedByU[col]];
+		public void Execute(double lambda, int numOfAlsIterations)
+		{
+			for (int k = 0; k < numOfAlsIterations; k++)
+			{
+				for (int u = 0; u < R.u; u++)
+				{
+					List<int> productsRatedByU = R.FindAllProductsRatedByUser(u);
 
-                        for (int row = 0; row < countOfFactors; row++) 
-                        {
-                            Vu.Data[row, 0] += rating * Piu.Data[row, col];
-                        }
-                    }
-                    // W tym miejscu Vu jest gotowe do użytku
-                    
-                    // Gauss statyczny : Krzychu
-                    Matrix X = Gauss.Calculate(Au, Vu);
+					Matrix Piu = new Matrix(countOfFactors, productsRatedByU.Count);
 
-                    for (int row = 0; row < countOfFactors; row++) 
-                    {
-                        U.Data[row, u] = X.Data[row, 0];
-                    }
-                }
-                
-                for (int p = 0; p < R.p; p++)
-                {
-                    List<int> usersWhoRatedP = R.FindAllUsersWhoRatedProduct(p);
-                    
-                    Matrix Uip = new Matrix(countOfFactors, usersWhoRatedP.Count);
+					for (int i = 0; i < productsRatedByU.Count; i++)
+					{
+						for (int row = 0; row < countOfFactors; row++)
+						{
+							Piu.Data[row, i] = P.Data[row, productsRatedByU[i]];
+						}
+					}
+					// W tym miejscu Piu jest gotowe do użytku
 
-                    for (int i = 0; i < usersWhoRatedP.Count; i++) 
-                    {
-                        for (int row = 0; row < countOfFactors; row++) 
-                        {
-                            Uip.Data[row, i] = U.Data[row, usersWhoRatedP[i]];
-                        }
-                    }
-                    // W tym miejscu Uip jest gotowe do użytku
-                    
-                    Matrix Bu = Uip * Uip.GetTransposed();
-                    Bu.AddLambdaMatrix(lambda);
-                    
-                    Matrix Wp = new Matrix(countOfFactors, 1);
+					Matrix Au = Piu * Piu.GetTransposed();
+					Au.AddLambdaMatrix(lambda);
 
-                    for (int col = 0; col < Uip.ColumnCount; col++) 
-                    {
-                        int rating = R[p, usersWhoRatedP[col]];
+					Matrix Vu = new Matrix(countOfFactors, 1);
 
-                        for (int row = 0; row < countOfFactors; row++) 
-                        {
-                            Wp.Data[row, 0] += rating * Uip.Data[row, col];
-                        }
-                    }
-                    // W tym miejscu Wp jest gotowe do użytku
-                    
-                    Matrix X = Gauss.Calculate(Bu, Wp);
+					for (int col = 0; col < Piu.ColumnCount; col++)
+					{
+						int rating = R[u, productsRatedByU[col]];
 
-                    for (int row = 0; row < countOfFactors; row++) 
-                    {
-                        P.Data[row, p] = X.Data[row, 0];
-                    }
-                }
-                
-                Console.WriteLine($"Iteracja {k + 1}) Wartość funkcji celu = {ObjectiveFunction.Calculate(R, U, P,0.1)}");
-            }
-        }
-    }
+						for (int row = 0; row < countOfFactors; row++)
+						{
+							Vu.Data[row, 0] += rating * Piu.Data[row, col];
+						}
+					}
+					// W tym miejscu Vu jest gotowe do użytku
+
+					// Gauss statyczny : Krzychu
+					Matrix X = Gauss.Calculate(Au, Vu);
+
+					for (int row = 0; row < countOfFactors; row++)
+					{
+						U.Data[row, u] = X.Data[row, 0];
+					}
+				}
+
+				for (int p = 0; p < R.p; p++)
+				{
+					List<int> usersWhoRatedP = R.FindAllUsersWhoRatedProduct(p);
+
+					Matrix Uip = new Matrix(countOfFactors, usersWhoRatedP.Count);
+
+					for (int i = 0; i < usersWhoRatedP.Count; i++)
+					{
+						for (int row = 0; row < countOfFactors; row++)
+						{
+							Uip.Data[row, i] = U.Data[row, usersWhoRatedP[i]];
+						}
+					}
+					// W tym miejscu Uip jest gotowe do użytku
+
+					Matrix Bu = Uip * Uip.GetTransposed();
+					Bu.AddLambdaMatrix(lambda);
+
+					Matrix Wp = new Matrix(countOfFactors, 1);
+
+					for (int col = 0; col < Uip.ColumnCount; col++)
+					{
+						int rating = R[p, usersWhoRatedP[col]];
+
+						for (int row = 0; row < countOfFactors; row++)
+						{
+							Wp.Data[row, 0] += rating * Uip.Data[row, col];
+						}
+					}
+					// W tym miejscu Wp jest gotowe do użytku
+
+					Matrix X = Gauss.Calculate(Bu, Wp);
+
+					for (int row = 0; row < countOfFactors; row++)
+					{
+						P.Data[row, p] = X.Data[row, 0];
+					}
+				}
+
+				Console.WriteLine($"Iteracja {k + 1}) Wartość funkcji celu = {ObjectiveFunction.Calculate(R, U, P, 0.1)}");
+			}
+		}
+	}
 }
 
--- src/RMatrix.cs
+++ src/RMatrix.cs
@@ -4,117 +4,119 @@ using System.Text;
 
 namespace RecommenderSystem
 {
-    //Można używać jak zwykłej macierzy dwuwymiarowej 
-    //  tj. [0,2] zwróci ocenę (1-5) dla u = 0 i p = 2, albo 0 jeśli tej oceny nie ma 
-    //  Tylko jak wyskoczy poza index to zwraca 0 a nie rzuca nullpointerem
-    //  może się to potem doda jak się komuś chce
+	//Można używać jak zwykłej macierzy dwuwymiarowej 
+	//  tj. [0,2] zwróci ocenę (1-5) dla u = 0 i p = 2, albo 0 jeśli tej oceny nie ma 
+	//  Tylko jak wyskoczy poza index to zwraca 0 a nie rzuca nullpointerem
+	//  może się to potem doda jak się komuś chce
 
-    class RMatrix
-    {
-        Dictionary<Tuple<int,int>, int> ratings;
-        public int u, p;
+	class RMatrix
+	{
+		Dictionary<Tuple<int, int>, int> ratings;
+		public int u, p;
 
-        public RMatrix()
-        {
-            ratings = new Dictionary<Tuple<int, int>, int>();
-        }
+		public RMatrix()
+		{
+			ratings = new Dictionary<Tuple<int, int>, int>();
+		}
 
-        public void setSize(int u, int p)
-        {
-            this.u = u;
-            this.p = p;
-        }
+		public void setSize(int u, int p)
+		{
+			this.u = u;
+			this.p = p;
+		}
 
-        public void Add(int u, int p, int r)
-        {
-            var temp = Tuple.Create(u, p);
-            if (!ratings.ContainsKey(temp))
-                ratings.Add(temp, r);
-            if (!ratings.ContainsKey(temp))
-            {
-                this.ratings.Add(temp, r);
-            }
+		public void Add(int u, int p, int r)
+		{
+			var temp = Tuple.Create(u, p);
 
-        }
+			if (!ratings.ContainsKey(temp))
+			{
+				ratings.Add(temp, r);
+			}
+		}
 
-        public int this[int u, int p] => Search(u, p);
-		
-        private int Search(int u, int p)
-        {
-            var search = Tuple.Create(u, p);
-            if (!ratings.ContainsKey(search))
-                return 0;
-            return ratings[search];
-        }
+		public int this[int u, int p] => Search(u, p);
 
-        public List<int> FindAllProductsRatedByUser(int u)
-        {
-            List<int> list = new List<int>();
+		private int Search(int u, int p)
+		{
+			var search = Tuple.Create(u, p);
+			if (!ratings.ContainsKey(search))
+			{
+				return 0;
+			}
+			return ratings[search];
+		}
 
-            for (int i = 0; i < p; i++)
-            {
-                if(Search(u, i) != 0)
-                {
-                    list.Add(i);
-                }
-            }
+		public List<int> FindAllProductsRatedByUser(int u)
+		{
+			List<int> list = new List<int>();
 
-            return list;
-        }
+			for (int i = 0; i < p; i++)
+			{
+				if (Search(u, i) != 0)
+				{
+					list.Add(i);
+				}
+			}
 
-        public List<int> FindAllUsersWhoRatedProduct(int p)
-        {
-            List<int> list = new List<int>();
+			return list;
+		}
 
-            for (int i = 0; i < u; i++)
-            {
-                if (Search(i, p) != 0)
-                {
-                    list.Add(i);
-                }
-            }
+		public List<int> FindAllUsersWhoRatedProduct(int p)
+		{
+			List<int> list = new List<int>();
 
-            return list;
-        }
-      
-        public Dictionary<Tuple<int, int>, int> PrepareToHidingTest(int percent)
-        {
-            var listOfHiddenRatings = new Dictionary<Tuple<int, int>, int>();
+			for (int i = 0; i < u; i++)
+			{
+				if (Search(i, p) != 0)
+				{
+					list.Add(i);
+				}
+			}
 
-            double total = 0;
+			return list;
+		}
 
-            foreach (KeyValuePair<Tuple<int, int>, int> rating in ratings)
-            {
-                if(total > 100)
-                {
-                    listOfHiddenRatings.Add(rating.Key, rating.Value);
-                    total -= 100;
-                }
-                total += percent;
-            }
+		public Dictionary<Tuple<int, int>, int> PrepareToHidingTest(int percent)
+		{
+			var listOfHiddenRatings = new Dictionary<Tuple<int, int>, int>();
 
-            foreach (KeyValuePair<Tuple<int, int>, int> rating in listOfHiddenRatings)
-            {
-                ratings.Remove(rating.Key);
-            }
+			double total = 0;
 
-            return listOfHiddenRatings;
-        }
+			foreach (KeyValuePair<Tuple<int, int>, int> rating in ratings)
+			{
+				if (total > 100)
+				{
+					listOfHiddenRatings.Add(rating.Key, rating.Value);
+					total -= 100;
+				}
+				total += percent;
+			}
 
-        public void CheckFillDegree()
-        {
-            double notEmpty = 0;
-            double all = 0;
-            for (int i = 0; i < u; i++)
-            {
-                for (int j = 0; j < p; j++)
-                {
-                    if (Search(i, j) != 0)
-                        notEmpty++;
-                    all++;
-                }
-            }
-            Console.WriteLine("\t{0:N2} % Matrix Filled (no duplicates)", notEmpty/all * 100);
-        }
-    }
+			foreach (KeyValuePair<Tuple<int, int>, int> rating in listOfHiddenRatings)
+			{
+				ratings.Remove(rating.Key);
+			}
+
+			return listOfHiddenRatings;
+		}
+
+		public void CheckFillDegree()
+		{
+			double notEmpty = 0, all = 0;
+
+			for (int i = 0; i < u; i++)
+			{
+				for (int j = 0; j < p; j++)
+				{
+					if (Search(i, j) != 0)
+					{
+						notEmpty++;
+					}
+					all++;
+				}
+			}
+			Console.WriteLine("\t{0:N2} % Matrix Filled (no duplicates)", notEmpty / all * 100);
+		}
+	}
 }
