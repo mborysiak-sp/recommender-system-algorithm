1e58227070b61f4dea8f208fbd4699d8f6750f8e
KordianCeranowski <49246447+KordianCeranowski@users.noreply.github.com>
2019-12-08 03:21:49 +0100

    Update AlsAlgorithm.cs

--- src/AlsAlgorithm.cs
+++ src/AlsAlgorithm.cs
@@ -26,52 +26,6 @@ public class AlsAlgorithm {
 		Matrix bP = b * b.GetTransposed();
 		bP.AddLambdaMatrix(21.37);
 	    //for (int i = 0; i < U; i++) {}
-
-
-
-
-
-
-
-
-
-
-
-
-	    /*
-	    Matrix a = new Matrix(3,1);
-	    a.Fill();
-	    Console.WriteLine($"A: \n{a}");
-			
-	    //Matrix x = new Matrix(3, 1);
-	    //x.Fill();
-			
-			
-	    Matrix aTr = a.GetTransposed();
-	    Console.WriteLine($"Transposed A: \n{aTr}");
-
-	    Matrix result = a * aTr;
-
-	    result.AddLambdaMatrix(21.37);
-	    Console.WriteLine($"Au: \n{result}");
-			
-	    
-	    GaussianElimination gaussianElimination = new GaussianElimination();
-
-	    Console.WriteLine($"Calculated X: {gaussianElimination.Calculate(a, b)}");
-
-	    Console.WriteLine($"Original X: {x}");
-	    
-			
-	    RMatrix R = Extractor.createR(10000, 100);
-
-	    var test1 = R.FindAllProductsRatedByUser(150);
-	    var test2 = R.FindAllUsersWhoRatedProduct(0);
-	    RMatrix R2 = R;
-
-	    var test3 = R.PrepareToHidingTest(2);
-	    Console.WriteLine();
-	    */
     }
 }
 }
