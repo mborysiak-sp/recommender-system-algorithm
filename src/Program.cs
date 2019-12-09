bacc27e9b959a5df2718a398a21f6c663f556d43
Krzysztof Bohdanowicz <kbohdanowicz@sigma.ug.edu.pl>
2019-12-09 16:45:36 +0100

    test selection system

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+// ReSharper disable InvalidXmlDocComment
 
 namespace RecommenderSystem
 {
@@ -6,20 +7,44 @@ namespace RecommenderSystem
     {
         private static void Main(string[] args) 
         {
+            //NOWATORSKI SYSTEM WYBIERANIA ILOŚCI TESTÓW
+            //WYSTARCZY ODKOMENTOWAĆ DANY ZAKRES
+            
             Parallel.Invoke(
+                ///*
+                
+                //dawka: 10000
                 () => new Test("TEST_1").Execute(3,10000,50,0.1, 10, 0.1)
-                /*
-                () => new Test("TEST_2").Execute(3,10000,50,0.1, 10, 0.1),
-                () => new Test("TEST_3").Execute(3,10000,1050,0.001, 10, 0.1),
+                ///*
+                ,
+                () => new Test("TEST_2").Execute(3,10000,500,0.1, 10, 0.1)
+                ///*
+                ,
+                () => new Test("TEST_3").Execute(3,10000,1050,0.1, 10, 0.1)
+                ///*
+                ,
                 
-                () => new Test("TEST_4").Execute(3,10000,50,0.1, 10, 0.1),
-                () => new Test("TEST_5").Execute(3,10000,50,0.1, 10, 0.1),
-                () => new Test("TEST_6").Execute(3,10000,50,0.1, 10, 0.1),
+                //dawka: 200000
+                () => new Test("TEST_4").Execute(3,200000,50,0.1, 10, 0.1)
+                ////*
+                ,
+                () => new Test("TEST_5").Execute(3,200000,50,0.1, 10, 0.1)
+                ///*
+                ,
+                () => new Test("TEST_6").Execute(3,200000,50,0.1, 10, 0.1)
                 
-                () => new Test("TEST_7").Execute(3,10000,50,0.1, 10, 0.1),
-                () => new Test("TEST_8").Execute(3,10000,50,0.1, 10, 0.1),
-                () => new Test("TEST_9").Execute(3,10000,50,0.1, 10, 0.1)
-                */
+                ///*
+                ,
+                
+                //dawka: 550000
+                () => new Test("TEST_7").Execute(3,550000,50,0.1, 10, 0.1)
+                ///*
+                ,
+                () => new Test("TEST_8").Execute(3,550000,50,0.1, 10, 0.1)
+                ///*
+                ,
+                () => new Test("TEST_9").Execute(3,550000,50,0.1, 10, 0.1)
+                //*/
             );
         }
     }
